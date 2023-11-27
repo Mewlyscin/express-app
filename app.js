636f11e81b9337e51843ff6a1e42f68957c418c3
Mewlyscin <Tear-Lyons7@live.fr>
2023-11-27 10:07:40 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require("express");
+
+const app = express();
+
+const port = 5000;
+
+const movies = [
+  {
+    id: 1,
+    title: "Citizen Kane",
+    director: "Orson Wells",
+    year: "1941",
+    color: false,
+    duration: 120,
+  },
+  {
+    id: 2,
+    title: "The Godfather",
+    director: "Francis Ford Coppola",
+    year: "1972",
+    color: true,
+    duration: 180,
+  },
+  {
+    id: 3,
+    title: "Pulp Fiction",
+    director: "Quentin Tarantino",
+    year: "1994",
+    color: true,
+    duration: 180,
+  },
+];
+
+app.get("/api/movies/:id", (req, res) => {
+  const movId = parseInt(req.params.id);
+
+  const movie = movies.find((movie) => movie.id === movId);
+
+  if (movie != null) {
+    res.json(movie);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+const getMovies = (req, res) => {
+  res.status(200).json(movies);
+};
+app.get("/api/movies", getMovies);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to my favourite movie list");
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    console.log(`Server is listening on ${port}`);
+  }
+});
