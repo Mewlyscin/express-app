01b0957c1062a8526c76c95121f5d23ed578fb0b
Mewlyscin <Tear-Lyons7@live.fr>
2023-11-27 12:17:05 +0100

    update/create dossier test /fichier movies.test and app

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const express = require("express");
 
 const app = express();
 
-const port = 5000;
-
 const movies = [
   {
     id: 1,
@@ -52,10 +50,4 @@ app.get("/", (req, res) => {
   res.send("Welcome to my favourite movie list");
 });
 
-app.listen(port, (err) => {
-  if (err) {
-    console.error("Something bad happened");
-  } else {
-    console.log(`Server is listening on ${port}`);
-  }
-});
+module.exports = app;
